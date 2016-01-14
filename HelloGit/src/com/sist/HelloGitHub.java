b5f8119ec977fe853f0cc526dc82119dee3cbfc1
daehyeonghwang <svbd42@naver.com>
2016-01-14 10:30:35 +0900

    added line Hello GitHub

---
+++ HelloGit/src/com/sist/HelloGitHub.java
@@ -0,0 +1,11 @@
+package com.sist;
+
+public class HelloGitHub {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello GitHub");
+		
+	}
+
+}
